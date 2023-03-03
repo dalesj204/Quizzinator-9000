151fbb51b22a1b1187c1028ad59747ed346c3970
Shawn C <xiaocheng.shawn@gmail.com>
2023-03-03 15:49:45 -0500

    Updates_Quizzes

--- quizzes/models.py
+++ quizzes/models.py
@@ -11,30 +11,48 @@ class SubjectTags(models.Model):
     def __str__(self):
         return self.name
 
-class QuestionBank(models.Model):
-    QUESTION_TYPES = (
-        ('mcq', 'Multiple Choice'),
-        ('pmcq', 'Permutation Multiple Choice'),
-        ('pp', "Parson's Problem")
-    )
-
-    question = models.CharField(max_length=500)
-    question_type = models.CharField(max_length=4, choices=QUESTION_TYPES)
-    correct_answer = models.CharField(max_length=500)
-    distractor_1 = models.CharField(max_length=500)
-    distractor_2 = models.CharField(max_length=500)
-    distractor_3 = models.CharField(max_length=500)
-
+class MultipleChoice(models.Model):
+    question = models.CharField(max_length=255)
+    choice1 = models.CharField(max_length=255)
+    choice2 = models.CharField(max_length=255)
+    choice3 = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+    
     def __str__(self):
         return self.question
+    
+
+class PermutationMultipleChoice(models.Model):
+    question = models.CharField(max_length=255)
+    choice1 = models.CharField(max_length=255)
+    choice2 = models.CharField(max_length=255)
+    choice3 = models.CharField(max_length=255)
+    choice4 = models.CharField(max_length=255)
+    correct_choices = models.ManyToManyField('self', symmetrical=False)
+    
+    def __str__(self):
+        return self.question
+    
+
+class ParsonsProblem(models.Model):
+    question = models.CharField(max_length=255)
+    code = models.TextField()
+    correct_answer = models.CharField(max_length=255)
+    distractor1 = models.CharField(max_length=255)
+    distractor2 = models.CharField(max_length=255)
+    distractor3 = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.question
+    
 
 class Quiz(models.Model):
-    name = models.CharField(max_length=100)
-    start_time = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    time_limit = models.PositiveIntegerField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quizzes')
-    questions = models.ManyToManyField(QuestionBank, related_name='quizzes')
-
+    time_limit = models.IntegerField()
+    questions = models.ManyToManyField('MultipleChoice', 'PermutationMultipleChoice', 'ParsonsProblem')
+    
     def __str__(self):
         return self.name
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,15 +1,95 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.http import HttpResponseBadRequest
-from .models import Quiz, QuestionBank
-from .forms import QuizForm, QuestionBankForm
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Quiz, MultipleChoice, PermutationMultipleChoice, ParsonsProblem
 
-# @login_required
-# def create_quiz(request):
-#     if request.method == 'POST':
-#         form = QuizForm(request.POST)
-#         if form.is_valid():
-#             quiz = form.save(commit=False)
-#             quiz.owner = request.user
-#             quiz.save()
+
+def quiz_list(request):
+    quizzes = Quiz.objects.filter(start_time__lte=timezone.now(), end_time__gte=timezone.now())
+    return render(request, 'quiz_list.html', {'quizzes': quizzes})
+
+
+@login_required
+def create_quiz(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        owner = request.user
+        start_time = timezone.now()
+        end_time = request.POST['end_time']
+        time_limit = request.POST['time_limit']
+        questions = request.POST.getlist('questions')
+        
+        quiz = Quiz(name=name, owner=owner, start_time=start_time, end_time=end_time, time_limit=time_limit)
+        quiz.save()
+        for question_id in questions:
+            try:
+                question = MultipleChoice.objects.get(id=question_id)
+                quiz.questions.add(question)
+            except ObjectDoesNotExist:
+                try:
+                    question = PermutationMultipleChoice.objects.get(id=question_id)
+                    quiz.questions.add(question)
+                except ObjectDoesNotExist:
+                    try:
+                        question = ParsonsProblem.objects.get(id=question_id)
+                        quiz.questions.add(question)
+                    except ObjectDoesNotExist:
+                        pass
+                
+        return redirect('quiz_list')
+    else:
+        questions = {
+            'multiple_choice': MultipleChoice.objects.all(),
+            'permutation_multiple_choice': PermutationMultipleChoice.objects.all(),
+            'parsons_problem': ParsonsProblem.objects.all
+        }
+    return render(request, 'create_quiz.html', {'questions': questions})
+
+@login_required
+def update_quiz(request, pk):
+    quiz = get_object_or_404(Quiz, pk=pk)
+    if request.method == 'POST':
+        quiz.name = request.POST['name']
+        quiz.end_time = request.POST['end_time']
+        quiz.time_limit = request.POST['time_limit']
+        quiz.questions.clear()
+        questions = request.POST.getlist('questions')
+        for question_id in questions:
+            try:
+                question = MultipleChoice.objects.get(id=question_id)
+                quiz.questions.add(question)
+            except ObjectDoesNotExist:
+                try:
+                    question = PermutationMultipleChoice.objects.get(id=question_id)
+                    quiz.questions.add(question)
+                except ObjectDoesNotExist:
+                    try:
+                        question = ParsonsProblem.objects.get(id=question_id)
+                        quiz.questions.add(question)
+                    except ObjectDoesNotExist:
+                        pass
+        quiz.save()
+        return redirect('quiz_list')
+    else:
+        questions = {
+            'multiple_choice': MultipleChoice.objects.all(),
+            'permutation_multiple_choice': PermutationMultipleChoice.objects.all(),
+            'parsons_problem': ParsonsProblem.objects.all(),
+        }
+        selected_questions = quiz.questions.all()
+        return render(request, 'update_quiz.html', {'quiz': quiz, 'questions': questions, 'selected_questions': selected_questions})
+
+@login_required
+def take_quiz(request, pk):
+    quiz = get_object_or_404(Quiz, pk=pk)
+    if request.method == 'POST':
+        # process the quiz submission
+        pass
+    else:
+        questions = quiz.questions.all()
+        context = {
+        'quiz': quiz,
+        'questions': questions,
+        }
+    return render(request, 'take_quiz.html', context)
